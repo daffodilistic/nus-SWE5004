374a18e4169d1d07b95a6bd878e5214865267860
Soh Thiam Hing <daffodilistic@gmail.com>
2023-03-31 17:47:47 +0800

    [feature (backend)] Add creation and retrieval API

---
+++ src/backend/README.md
@@ -0,0 +1,4 @@
+# MarioToilet Backend
+How to run:
+1. Install python packages from requirements.txt
+2. Run the server with `python -m uvicorn main:app --reload`
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,36 +1,39 @@
+import json
 from typing import Union
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from bson.json_util import dumps
 
-from models import User, Base
-
-# In-memory database
-engine = create_engine("sqlite:///mario_toilet.sqlite", echo=True)
-Base.metadata.create_all(engine)
+import datetime
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    with Session(engine) as session:
-        # p = User(name='Kate', email="kate@sharklasers.com", password="123456")
-        # commit() will be done automatically
-        # database session cache will be cleared automatically
-        # database connection will be returned to the pool
-        # session.add_all([p])
-        # session.commit()
-
-        stmt = select(User)
-
-        users = []
-        for user in session.scalars(stmt):
-            users.append(user)
-        return users
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/users")
+async def create_user(request: Request):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['mariotoilet']
+    users:Collection = db['users']
+    user = await request.json()
+    user_id = users.insert_one(user)
+    user = users.find_one({"_id": user_id.inserted_id})
+    user["_id"] = str(user.pop("_id"))
+    return {"user": user}
+
+@app.get("/users")
+async def retrieve_user():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['mariotoilet']
+    users:Collection = db['users']
+    user_list = list(users.find({}))
+    for user in user_list:
+        user["_id"] = str(user.pop("_id"))
+    return user_list
