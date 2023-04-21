4378e919e54b61e2560463769fd01786d12ce9e1
Soh Thiam Hing <daffodilistic@gmail.com>
2023-04-21 17:19:29 +0800

    [feature (device enrollment)] Verify device data serialization and deserialization

--- src/backend/main.py
+++ src/backend/main.py
@@ -31,38 +31,45 @@ def read_root():
 async def create_user(request: Request):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['mariotoilet']
-    users:Collection = db['users']
+    users: Collection = db['users']
     user = await request.json()
     user_id = users.insert_one(user)
     user = users.find_one({"_id": user_id.inserted_id})
     user["_id"] = str(user.pop("_id"))
     return {"user": user}
 
+
 @app.get("/users")
 async def retrieve_user():
     client = MongoClient('mongodb://localhost:27017/')
     db = client['mariotoilet']
-    users:Collection = db['users']
+    users: Collection = db['users']
     user_list = list(users.find({}))
     for user in user_list:
         user["_id"] = str(user.pop("_id"))
     return user_list
 
+
 @app.get("/api/devices/enroll")
 def enroll_device():
-    test_device_a = Device(RandMac(), datetime.datetime.now() , datetime.datetime.now(), make_client_secret())
     device_schema = DeviceSchema()
-    data = device_schema.dump(test_device_a)
-
-    test_device_b = {
+    test_device_params = {
         "mac_address": str(RandMac()),
         "last_seen": datetime.datetime.now().isoformat(),
         "first_seen": datetime.datetime.now().isoformat(),
         "client_secret": make_client_secret()
     }
-    print(device_schema.load(test_device_b))
 
-    return data
+    device = device_schema.load(test_device_params)
+    device_dump = device_schema.dump(device)
+
+    print("Deserialization Test:")
+    print(device)
+    print("Serialization Test:")
+    print(device_dump)
+
+    return device_dump
+
 
 def make_client_secret() -> str:
     return secrets.token_urlsafe(16)
